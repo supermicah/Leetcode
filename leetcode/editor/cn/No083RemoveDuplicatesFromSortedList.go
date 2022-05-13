7c7a80136d34bf1c4c3b1adb06fe2369b7d46751
micahshi <micahshi@tencent.com>
2022-05-13 15:21:32 +0800

    submit

---
+++ leetcode/editor/cn/No083RemoveDuplicatesFromSortedList.go
@@ -0,0 +1,74 @@
+package main
+
+import "fmt"
+
+//给定一个已排序的链表的头 head ， 删除所有重复的元素，使每个元素只出现一次 。返回 已排序的链表 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [1,1,2]
+//输出：[1,2]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点数目在范围 [0, 300] 内
+// -100 <= Node.val <= 100
+// 题目数据保证链表已经按升序 排列
+//
+// Related Topics 链表 👍 787 👎 0
+
+func main() {
+	head := &ListNode{
+		Val: 1,
+	}
+	head.Next = &ListNode{Val: 1}
+	head.Next.Next = &ListNode{Val: 2}
+	head.Next.Next.Next = &ListNode{Val: 3}
+	value := deleteDuplicates(head)
+	fmt.Println(fmt.Sprintf("%+v", value))
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (l *ListNode) String() string {
+	return fmt.Sprintf("%d, next: %s", l.Val, l.Next)
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	current := head
+	for current != nil && current.Next != nil {
+		for current.Next != nil && current.Val == current.Next.Val {
+			current.Next = current.Next.Next
+		}
+		current = current.Next
+	}
+	return head
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
